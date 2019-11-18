83aee1397682e261bc3045430699d0360b54a110
Mondira Pal <mondira.p@encoders.co.in>
2019-11-18 11:17:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/democam/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.democam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+    private static final int PICK_IMAGE=1;
+    Uri imageuri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+        imageView = (ImageView) this.findViewById(R.id.imageView1);
+        Button galleryButton=findViewById(R.id.button2);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent gallery=new Intent();
+                gallery.setType("Image");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery,"select picture"),PICK_IMAGE);
+            }
+        });
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+        if (requestCode==PICK_IMAGE && resultCode==RESULT_OK)
+        {
+            imageuri =data.getData();
+            try
+            {
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageuri);
+                imageView.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
